ccd283cbf72ba9060f4593399ac0dbd69533e71b
NaufalFadhilah <Naufalfadhilah689@gmail.com>
2022-12-20 22:19:49 +0800

    first commit

---
+++ kalkulator.js
@@ -0,0 +1,19 @@
+// insert value
+function insert(num) {
+  document.form.textarea.value = document.form.textarea.value + num;
+}
+// count
+function count() {
+  let result = document.form.textarea.value;
+  document.form.textarea.value = eval(result);
+}
+// clear
+function clean() {
+  document.form.textarea.value = '';
+}
+
+// delete
+function del() {
+  let result = document.form.textarea.value;
+  document.form.textarea.value = result.substring(0, result.length - 1);
+}
